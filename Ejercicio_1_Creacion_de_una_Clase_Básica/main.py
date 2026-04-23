27352f3e436e8bde9eaf639b13275ea0a88fa711
Johan Rojas <aldebaran.ex@gmail.com>
2026-04-23 07:53:18 -0500

    Correcion de nombres y se agrega "main.py" en todos los ejercicios

---
+++ Ejercicio_1_Creacion_de_una_Clase_Básica/main.py
@@ -0,0 +1,14 @@
+import Creacion_de_una_Clase_Básica
+
+# crear una instancia de la clase Carro
+mi_carro = Creacion_de_una_Clase_Básica.Carro("Toyota", "Corolla", 2020)
+# crear una segunda instancia de la clase Carro
+mi_carro1 = Creacion_de_una_Clase_Básica.Carro("Honda", "Camry", 2021)
+# crear una tercera instancia de la clase Carro
+mi_carro2 = Creacion_de_una_Clase_Básica.Carro("Ford", "Mustang", 2022)
+
+
+# mostrar la información del carro
+mi_carro.describir() # muestra la información del primer carro
+mi_carro1.describir() # muestra la información del segundo carro
+mi_carro2.describir() # muestra la información del tercer carro
--- Ejercicio_2_Encapsulamiento_y_Modificadores_de_Acceso/Encapsulamiento_y_Modificadores_de_Acceso.py
+++ Ejercicio_2_Encapsulamiento_y_Modificadores_de_Acceso/Encapsulamiento_y_Modificadores_de_Acceso.py
@@ -26,16 +26,3 @@ class Carro:
         
         def get_año(self): # método para obtener el año del carro
             return self.__año # devuelve el año del carro
-        
-# crear una instancia de la clase Carro
-mi_carro = Carro("Toyota", "Corolla", 2020)
-# crear una segunda instancia de la clase Carro
-mi_carro1 = Carro("Honda", "Camry", 2021)
-# crear una tercera instancia de la clase Carro
-mi_carro2 = Carro("Ford", "Mustang", 2022)
-
-
-# mostrar la información del carro
-mi_carro.describir() # muestra la información del primer carro
-mi_carro1.describir() # muestra la información del segundo carro
-mi_carro2.describir() # muestra la información del tercer carro 
